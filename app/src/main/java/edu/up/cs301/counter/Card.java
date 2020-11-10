74cf9cd5edb71e5d742dc9c6c8af2be7eca121c9
vidgenious <63082621+vidgenious@users.noreply.github.com>
2020-11-10 07:37:53 -0800

    Card Class added CounterState -> GuillotineState
    GuillotineState 1.0
    Card 1.0

---
+++ app/src/main/java/edu/up/cs301/counter/Card.java
@@ -0,0 +1,31 @@
+package edu.up.cs301.counter;
+
+/**
+ * @author William Cloutier
+ * @author Moses Karemera
+ * @author Maxwell McAtee
+ */
+
+/*
+ * This class is very bare bones and not in its final state. It only has the information
+ * necessary for part d
+ */
+public class Card {
+    //Instance variables for a card object
+    public boolean isNoble;
+    public boolean hasEffect;
+    public int points;
+    public String cardColor;
+    public String id;
+
+    //Constructor for Card object
+    public Card(boolean isNoble, boolean hasEffect, int points, String cardColor,  String id){
+        this.isNoble = isNoble;
+        this.hasEffect = hasEffect;
+        this.points = points;
+        this.cardColor = cardColor;
+        this.id = id;
+    }
+
+    public String getId(){ return this.id;}
+}
