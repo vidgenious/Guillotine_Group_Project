03bd1d2630307facddb477df7748c1e6a103627f
vidgenious <63082621+vidgenious@users.noreply.github.com>
2020-11-10 08:06:09 -0800

    bare bones for: GuillotineHumanPlayer GuillotineLocalGame GuillotineMainActivity

--- app/src/main/java/edu/up/cs301/counter/GuillotineHumanPlayer.java
+++ app/src/main/java/edu/up/cs301/counter/GuillotineHumanPlayer.java
@@ -1,4 +1,41 @@
 package edu.up.cs301.counter;
 
-public class GuillotineHumanPlayer {
+import android.view.MotionEvent;
+import android.view.View;
+
+import edu.up.cs301.game.GameFramework.GameHumanPlayer;
+import edu.up.cs301.game.GameFramework.GameMainActivity;
+import edu.up.cs301.game.GameFramework.infoMessage.GameInfo;
+import edu.up.cs301.game.R;
+
+public class GuillotineHumanPlayer extends GameHumanPlayer implements View.OnTouchListener {
+    private GuillotineState state;
+
+    private GameMainActivity myActivity;
+
+    public GuillotineHumanPlayer(String name) {super(name);}
+
+    public View getTopView() { return myActivity.findViewById(R.id.top_gui_layout);}
+
+    protected void updateDisplay() {
+
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        return false;
+    }
+
+    @Override
+    public void receiveInfo(GameInfo info) {
+        if (!(info instanceof GuillotineState)) return;
+
+        this.state = (GuillotineState)info;
+        updateDisplay();
+    }
+
+    @Override
+    public void setAsGui(GameMainActivity activity) {
+
+    }
 }
--- app/src/main/java/edu/up/cs301/counter/GuillotineLocalGame.java
+++ app/src/main/java/edu/up/cs301/counter/GuillotineLocalGame.java
@@ -1,4 +1,34 @@
 package edu.up.cs301.counter;
 
-public class GuillotineLocalGame {
+import edu.up.cs301.game.GameFramework.GamePlayer;
+import edu.up.cs301.game.GameFramework.LocalGame;
+import edu.up.cs301.game.GameFramework.actionMessage.GameAction;
+
+public class GuillotineLocalGame extends LocalGame {
+
+    private GuillotineState gameState;
+
+    @Override
+    protected boolean canMove(int playerIdx) {
+        return false;
+    }
+
+    public GuillotineLocalGame() {
+
+    }
+
+    @Override
+    protected boolean makeMove(GameAction action){
+        return false;
+    }
+
+    @Override
+    protected void sendUpdatedStateTo(GamePlayer p) {
+        p.sendInfo(new GuillotineState(gameState));
+    }
+
+    @Override
+    protected String checkIfGameOver() {
+        return null;
+    }
 }
