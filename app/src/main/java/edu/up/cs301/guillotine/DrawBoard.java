c0b8c8424013b41b5b36d4db8773adee42091fe4
mosesndagijimana <ndagijim22@up.edu>
2020-11-23 21:28:45 -0800

    Fixed UI and added other stuff.

--- app/src/main/java/edu/up/cs301/guillotine/DrawBoard.java
+++ app/src/main/java/edu/up/cs301/guillotine/DrawBoard.java
@@ -32,7 +32,7 @@ public class DrawBoard extends FlashSurfaceView {
     public DrawBoard(Context context, AttributeSet attrs) {
         super(context, attrs);
         setWillNotDraw(false);
-        grey.setColor(Color.GRAY);
+        grey.setColor(Color.WHITE);
         black.setColor(Color.BLACK);
         black.setTextSize(50.0f);
         choice.setColor(Color.RED);
@@ -50,56 +50,59 @@ public class DrawBoard extends FlashSurfaceView {
         if(state == null){
             return;
         }
-       //canvas.setBackgroundColor()
+
         canvas.drawRect(10.0f, 970.0f, 170.0f, 1070.0f, grey);
         canvas.drawText("Skip", 30.0f,1030.0f, black);
         canvas.drawRect(10.0f, 860.0f, 170.0f, 960.0f, grey);
         canvas.drawText("Accept", 10.0f,930.0f, black);
 
         grey.setTextSize(50.0f);
-        canvas.drawText("Player 1: " + state.getP1Score(), 10.0f, 400.0f, grey);
-        canvas.drawText("Player 0: " + state.getP0Score(), 10.0f, 500.0f, grey);
+        canvas.drawText("Player 1: " + state.getP1Score(), 10.0f, 200.0f, grey);
+        canvas.drawText("Player 0: " + state.getP0Score(), 10.0f, 300.0f, grey);
+        //canvas.drawText("Phase : " + state.getP0Score(), 10.0f, 400.0f, grey);
 
         grey.setTextSize(50.0f);
         canvas.drawText("GUILLOTINE GAME ", 10.0f, 100.0f, grey);
 
 
-        //P0 Hand 200,  280
+        //P0 Hand
         float left = 1700;
         Bitmap draw;
         for(int i = 0; i < state.getP0Hand().size(); i++){
             draw = BitmapFactory.decodeResource(getResources(), state.getP0Hand().get(i).image);
-            draw = Bitmap.createScaledBitmap(draw, 300, 420, true);
-            canvas.drawBitmap(draw, left, 900.0f, null); //800
-            left -= 220;
+            draw = Bitmap.createScaledBitmap(draw, 200, 280, true);
+            canvas.drawBitmap(draw, left, 800.0f, null); //800
+            left -= 220.0f;
         }
 
-        //P0 field 100 140
+        //P0 field
         left = 1800;
         for(int i = 0; i < state.getP0Field().size(); i++){
             draw = BitmapFactory.decodeResource(getResources(), state.getP0Field().get(i).image);
-            draw = Bitmap.createScaledBitmap(draw, 150, 210, true);
-            canvas.drawBitmap(draw, left, 675.0f, null); // 650
+            draw = Bitmap.createScaledBitmap(draw, 100, 140, true);
+            canvas.drawBitmap(draw, left, 650.0f, null); // 650
             left -= 120;
         }
 
-        //Noble line 200 280
+        //Noble line
         left = 1700;
         for(int i = 0; i < state.getNobleLine().size(); i++){
             draw = BitmapFactory.decodeResource(getResources(), state.getNobleLine().get(i).image);
-            draw = Bitmap.createScaledBitmap(draw, 300, 420, true);
-            canvas.drawBitmap(draw, left, 250.0f, null); //350
+            draw = Bitmap.createScaledBitmap(draw, 200, 280, true);
+            canvas.drawBitmap(draw, left, 350.0f, null); //350
             left -= 100;
         }
 
-        //P1 field 100   140
+        //P1 field
         left = 1800;
         for(int i = 0; i < state.getP1Field().size(); i++){
             draw = BitmapFactory.decodeResource(getResources(), state.getP1Field().get(i).image);
-            draw = Bitmap.createScaledBitmap(draw, 150, 210, true);
-            canvas.drawBitmap(draw, left, 35.0f, null); //150
+            draw = Bitmap.createScaledBitmap(draw, 100, 140, true);
+            canvas.drawBitmap(draw, left, 150.0f, null); //150
             left -= 120;
         }
+
+
 
         if(state.getTurnPhase() == 4){
             canvas.drawText("1", 50.0f, 400.0f, choice);
@@ -111,6 +114,35 @@ public class DrawBoard extends FlashSurfaceView {
             canvas.drawText("2", 720.0f, 400.0f, choice);
             canvas.drawText("3", 1375.0f, 400.0f, choice);
         }
+
+        // display the phases on the screen
+
+        if(state.getTurnPhase() == 0){
+            canvas.drawText("Action Card Phase", 500.0f, 50.0f, grey);
+        }
+
+        if(state.getTurnPhase() == 1){
+            canvas.drawText("Take Noble Phase", 500.0f, 50.0f, grey);
+        }
+        if(state.getTurnPhase() == 2){
+            canvas.drawText("Draw card Phase", 500.0f, 50.0f, grey );
+        }
+        if(state.getTurnPhase() == 3){
+            canvas.drawText("Select Noble in Line Phase", 500.0f, 50.0f, grey);
+        }
+        if(state.getTurnPhase() == 4){
+            canvas.drawText("Select ", 500.0f, 50.0f, grey);
+        }
+        if(state.getTurnPhase() == 5){
+            canvas.drawText("Select", 500.0f, 50.0f, grey);
+        }
+        if(state.getTurnPhase() == 6){
+            canvas.drawText("Select from Opponent ", 500.0f, 50.0f, grey);
+        }
+
+
+
+
 
     }
 }
