4b3425da3017d1475f4a893c63a75bbaba0dfc93
William <cloutier23@up.edu>
2020-12-03 20:46:43 -0800

    Fixed Smart AI to be player 0 and player 1

--- app/src/main/java/edu/up/cs301/guillotine/GuillotineComputerPlayer2.java
+++ app/src/main/java/edu/up/cs301/guillotine/GuillotineComputerPlayer2.java
@@ -29,31 +29,34 @@ public class GuillotineComputerPlayer2 extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
-        if(!(info instanceof GuillotineState)){
+        if (!(info instanceof GuillotineState)) {
             return;
         }
 
         Random rand = new Random();
         GuillotineState gameState = (GuillotineState) info;
 
-        for(int k = 0; k < gameState.getP1Hand().size(); k++){
-            if(gameState.getP1Hand().get(k).getId().equals("Scarlet")){
+        //check if player is position 1
+        if(this.playerNum == 1){
+
+        for (int k = 0; k < gameState.getP1Hand().size(); k++) {
+            if (gameState.getP1Hand().get(k).getId().equals("Scarlet")) {
                 this.hasScarlet = true;
             }
         }
 
         //code for if AI is player 1
-        if(gameState.getPlayerTurn() == 1) {
-            if(gameState.getTurnPhase() == 0) {
+        if (gameState.getPlayerTurn() == 1) {
+            if (gameState.getTurnPhase() == 0) {
 
 
                 int actionPos = 0;
                 int play = rand.nextInt(2);
                 int pos = rand.nextInt(gameState.getP1Hand().size());
                 //checks if it has the scarlet card and plays it if it has more points or if day is 3
-                if(hasScarlet && (gameState.getP1Score()-5 > gameState.getP0Score() || gameState.getP1Score() > gameState.getP0Score() && gameState.getDayNum() == 3)){
-                    for(int k = 0; k < gameState.getP1Hand().size(); k++){
-                        if(gameState.getP1Hand().get(k).getId().equals("Scarlet")){
+                if (hasScarlet && (gameState.getP1Score() - 5 > gameState.getP0Score() || gameState.getP1Score() > gameState.getP0Score() && gameState.getDayNum() == 3)) {
+                    for (int k = 0; k < gameState.getP1Hand().size(); k++) {
+                        if (gameState.getP1Hand().get(k).getId().equals("Scarlet")) {
                             actionPos = k;
                         }
                     }
@@ -64,8 +67,8 @@ public class GuillotineComputerPlayer2 extends GameComputerPlayer {
 
                 //checks if first card in noble line is bad, if so, change it if possible
                 else if ((!gameState.getNobleLine().isEmpty()) && gameState.getNobleLine().get(0).points < 3 && !gameState.getNobleLine().get(0).hasEffect) {
-                    for(int i = 0; i < gameState.getP1Hand().size(); i++){
-                        if(gameState.getP1Hand().get(i).affectsLine){
+                    for (int i = 0; i < gameState.getP1Hand().size(); i++) {
+                        if (gameState.getP1Hand().get(i).affectsLine) {
                             actionPos = i;
 
                         }
@@ -77,9 +80,9 @@ public class GuillotineComputerPlayer2 extends GameComputerPlayer {
 
 
                 //checks if next card in line is good, if so, change it if possible so next player cant get a good card
-                else if (gameState.getNobleLine().size() > 0 && (gameState.getNobleLine().get(1).points > 3 || gameState.getNobleLine().get(1).hasEffect)){
-                    for(int i = 0; i < gameState.getP1Hand().size(); i++){
-                        if(gameState.getP1Hand().get(i).affectsLine){
+                else if (gameState.getNobleLine().size() > 0 && (gameState.getNobleLine().get(1).points > 3 || gameState.getNobleLine().get(1).hasEffect)) {
+                    for (int i = 0; i < gameState.getP1Hand().size(); i++) {
+                        if (gameState.getP1Hand().get(i).affectsLine) {
                             actionPos = i;
 
                         }
@@ -91,14 +94,13 @@ public class GuillotineComputerPlayer2 extends GameComputerPlayer {
 
 
                 //AI randomly either plays an action card or skips
-               else if (play == 1 && gameState.getNobleLine().get(0).points < 3 && !gameState.getNobleLine().get(0).hasEffect) {
+                else if (play == 1 && gameState.getNobleLine().get(0).points < 3 && !gameState.getNobleLine().get(0).hasEffect) {
 
 
                     game.sendAction(new PlayAction(this, pos));
 
                     gameState.setTurnPhase(1);
-                }
-                else {
+                } else {
                     game.sendAction(new SkipAction(this));
                     gameState.setTurnPhase(1);
 
@@ -106,16 +108,16 @@ public class GuillotineComputerPlayer2 extends GameComputerPlayer {
             }
 
             //plays a noble action
-            else if(gameState.getTurnPhase() == 1){
+            else if (gameState.getTurnPhase() == 1) {
                 game.sendAction(new NobleAction(this));
             }
             //ai draws a card
-            else if(gameState.getTurnPhase() == 2){
+            else if (gameState.getTurnPhase() == 2) {
                 game.sendAction(new DrawAction(this));
             }
 
             //ai picks a second card
-            else if(gameState.getTurnPhase() == 3) {
+            else if (gameState.getTurnPhase() == 3) {
 
                 ChooseAction action = new ChooseAction(this, this.pos, 1);
                 game.sendAction(action);
@@ -123,39 +125,153 @@ public class GuillotineComputerPlayer2 extends GameComputerPlayer {
             }
 
             //picking a distance
-            else if(gameState.getTurnPhase() == 4){
+            else if (gameState.getTurnPhase() == 4) {
                 int chos = rand.nextInt(2) + 1;
 
                 ChooseAction action = new ChooseAction(this, chos, 2);
                 game.sendAction(action);
 
-            }
-            else if (gameState.getTurnPhase() == 5){
+            } else if (gameState.getTurnPhase() == 5) {
                 int chos = rand.nextInt(3) + 1;
 
                 ChooseAction action = new ChooseAction(this, chos, 2);
                 game.sendAction(action);
 
-            }
-            else if (gameState.getTurnPhase() == 6){
+            } else if (gameState.getTurnPhase() == 6) {
                 int chos = rand.nextInt(gameState.getP0Hand().size());
 
                 ChooseAction action = new ChooseAction(this, chos, 1);
                 game.sendAction(action);
-            }
-            else if (gameState.getTurnPhase() == 7){
+            } else if (gameState.getTurnPhase() == 7) {
                 int chos = rand.nextInt(gameState.getDeckDiscard().size());
 
                 ChooseAction action = new ChooseAction(this, chos, 1);
                 game.sendAction(action);
             }
-
+            else{
+                game.sendAction(new NullAction(this));
+            }
         }
+    }//end check is see if AI Is player 1
 
-
+        //for when AI is position 0
         else{
-            game.sendAction(new NullAction(this));
-        }
+            for (int k = 0; k < gameState.getP0Hand().size(); k++) {
+                if (gameState.getP0Hand().get(k).getId().equals("Scarlet")) {
+                    this.hasScarlet = true;
+                }
+            }
+
+            //code for if AI is player 1
+            if (gameState.getPlayerTurn() == 0) {
+                if (gameState.getTurnPhase() == 0) {
+
+
+                    int actionPos = 0;
+                    int play = rand.nextInt(2);
+                    int pos = rand.nextInt(gameState.getP0Hand().size());
+                    //checks if it has the scarlet card and plays it if it has more points or if day is 3
+                    if (hasScarlet && (gameState.getP0Score() - 5 > gameState.getP1Score() || gameState.getP0Score() > gameState.getP1Score() && gameState.getDayNum() == 3)) {
+                        for (int k = 0; k < gameState.getP0Hand().size(); k++) {
+                            if (gameState.getP0Hand().get(k).getId().equals("Scarlet")) {
+                                actionPos = k;
+                            }
+                        }
+                        this.pos = 0;
+                        game.sendAction(new PlayAction(this, actionPos));
+                        gameState.setTurnPhase(1);
+                    }
+
+                    //checks if first card in noble line is bad, if so, change it if possible
+                    else if ((!gameState.getNobleLine().isEmpty()) && gameState.getNobleLine().get(0).points < 3 && !gameState.getNobleLine().get(0).hasEffect) {
+                        for (int i = 0; i < gameState.getP0Hand().size(); i++) {
+                            if (gameState.getP0Hand().get(i).affectsLine) {
+                                actionPos = i;
+
+                            }
+                        }
+                        this.pos = 0;
+                        game.sendAction(new PlayAction(this, actionPos));
+                        gameState.setTurnPhase(1);
+                    }
+
+
+                    //checks if next card in line is good, if so, change it if possible so next player cant get a good card
+                    else if (gameState.getNobleLine().size() > 0 && (gameState.getNobleLine().get(1).points > 3 || gameState.getNobleLine().get(1).hasEffect)) {
+                        for (int i = 0; i < gameState.getP0Hand().size(); i++) {
+                            if (gameState.getP0Hand().get(i).affectsLine) {
+                                actionPos = i;
+
+                            }
+                        }
+                        this.pos = 1;
+                        game.sendAction(new PlayAction(this, actionPos));
+                        gameState.setTurnPhase(1);
+                    }
+
+
+                    //AI randomly either plays an action card or skips
+                    else if (play == 1 && gameState.getNobleLine().get(0).points < 3 && !gameState.getNobleLine().get(0).hasEffect) {
+
+
+                        game.sendAction(new PlayAction(this, pos));
+
+                        gameState.setTurnPhase(1);
+                    } else {
+                        game.sendAction(new SkipAction(this));
+                        gameState.setTurnPhase(1);
+
+                    }
+                }
+
+                //plays a noble action
+                else if (gameState.getTurnPhase() == 1) {
+                    game.sendAction(new NobleAction(this));
+                }
+                //ai draws a card
+                else if (gameState.getTurnPhase() == 2) {
+                    game.sendAction(new DrawAction(this));
+                }
+
+                //ai picks a second card
+                else if (gameState.getTurnPhase() == 3) {
+
+                    ChooseAction action = new ChooseAction(this, this.pos, 1);
+                    game.sendAction(action);
+
+                }
+
+                //picking a distance
+                else if (gameState.getTurnPhase() == 4) {
+                    int chos = rand.nextInt(2) + 1;
+
+                    ChooseAction action = new ChooseAction(this, chos, 2);
+                    game.sendAction(action);
+
+                } else if (gameState.getTurnPhase() == 5) {
+                    int chos = rand.nextInt(3) + 1;
+
+                    ChooseAction action = new ChooseAction(this, chos, 2);
+                    game.sendAction(action);
+
+                } else if (gameState.getTurnPhase() == 6) {
+                    int chos = rand.nextInt(gameState.getP1Hand().size());
+
+                    ChooseAction action = new ChooseAction(this, chos, 1);
+                    game.sendAction(action);
+                } else if (gameState.getTurnPhase() == 7) {
+                    int chos = rand.nextInt(gameState.getDeckDiscard().size());
+
+                    ChooseAction action = new ChooseAction(this, chos, 1);
+                    game.sendAction(action);
+                }
+
+            }//if turn is 0
+            else{
+                game.sendAction(new NullAction(this));
+            }
+        } //end when AI is position 0
+
     }
 }
 
